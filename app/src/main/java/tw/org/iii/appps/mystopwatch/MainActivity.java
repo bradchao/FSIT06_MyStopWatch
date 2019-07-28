52f90885262857a1ec0df4db214e8d2537a960a4
Brad <brad@brad.tw>
2019-07-28 10:32:13 +0800

    04

--- app/src/main/java/tw/org/iii/appps/mystopwatch/MainActivity.java
+++ app/src/main/java/tw/org/iii/appps/mystopwatch/MainActivity.java
@@ -1,27 +1,38 @@
 package tw.org.iii.appps.mystopwatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.strictmode.UnbufferedIoViolation;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private boolean isRunning;
+    private TextView clock;
     private Button leftBtn, rightBtn;
     private Timer timer;
     private CounterTask task;
     private int iCounter;
+    private UIHandler uiHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        clock = findViewById(R.id.clock);
+
         timer = new Timer();
+        uiHandler = new UIHandler();
 
         leftBtn = findViewById(R.id.leftBtn);
         rightBtn = findViewById(R.id.rightBtn);
@@ -41,9 +52,24 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             iCounter++;
+            uiHandler.sendEmptyMessage(0);
         }
     }
 
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            clock.setText(toClockString(iCounter));
+        }
+    }
+
+    private static String toClockString(int i){
+        int hs = i % 100;
+        int ts = i / 100;
+        
+        return ts + "." + hs;
+    }
 
     public void doLeft(View view) {
     }
@@ -56,6 +82,8 @@ public class MainActivity extends AppCompatActivity {
         if (isRunning){
             task = new CounterTask();
             timer.schedule(task, 10, 10);
+        }else if (task != null){
+            task.cancel();
         }
     }
 }
