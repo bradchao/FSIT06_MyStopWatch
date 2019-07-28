62a3f71210c5036ca339c66c31269c6e495e27ab
Brad <brad@brad.tw>
2019-07-28 10:13:46 +0800

    03

--- app/src/main/java/tw/org/iii/appps/mystopwatch/MainActivity.java
+++ app/src/main/java/tw/org/iii/appps/mystopwatch/MainActivity.java
@@ -6,18 +6,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
     private boolean isRunning;
     private Button leftBtn, rightBtn;
+    private Timer timer;
+    private CounterTask task;
+    private int iCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        timer = new Timer();
+
         leftBtn = findViewById(R.id.leftBtn);
         rightBtn = findViewById(R.id.rightBtn);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (timer != null){
+            timer.cancel();
+            timer.purge();
+            timer = null;
+        }
+    }
+
+    private class CounterTask extends TimerTask {
+        @Override
+        public void run() {
+            iCounter++;
+        }
+    }
+
 
     public void doLeft(View view) {
     }
@@ -26,5 +52,10 @@ public class MainActivity extends AppCompatActivity {
         isRunning = !isRunning;
         leftBtn.setText(isRunning?"Lap":"Reset");
         rightBtn.setText(isRunning?"Stop":"Start");
+
+        if (isRunning){
+            task = new CounterTask();
+            timer.schedule(task, 10, 10);
+        }
     }
 }
